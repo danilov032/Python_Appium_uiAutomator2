7c3ac0affa9a7cb18014e38dcd0d23bac9c010e6
danilov.and <danilov032.andrey@gmail.com>
2021-07-15 16:36:43 +0300

    Правки

--- AppiumScript.py
+++ AppiumScript.py
@@ -7,7 +7,11 @@ desired_capabilities = {
     "deviceName": "Test",
     "platformName": "Android",
     "platformVersion": "11",
-    "app": "/Users/danilov.and/Documents/Python_Appium_uiAutomator2/app/1xBet_base_cacheFile-11-6977177.apk",
+    "app": "/Users/danilov.and/Documents/Python_Appium_uiAutomator2/app/1xBet_base.apk",
+    "noReset": "true",
+    "unicodeKeyboard": "true",
+    "useNewWDA": "false",
+    "usePrebuiltWDA": "true"
 }
 
 driver = webdriver.Remote('http://127.0.0.1:5905/wd/hub', desired_capabilities=desired_capabilities)
@@ -15,12 +19,12 @@ driver = webdriver.Remote('http://127.0.0.1:5905/wd/hub', desired_capabilities=d
 
 driver.implicitly_wait(10)
 
-time.sleep(10)
+time.sleep(5)
 
 print("Жмяк")
 driver.find_elements_by_class_name("android.widget.ImageButton")[0].click()
-driver.find_elements_by_id("logout_item_drawer").click()
+driver.find_element_by_id("logout_item_drawer").click()
 time.sleep(3)
-driver.find_elements_by_id("button2")
+driver.find_element_by_id("button2")
 
 time.sleep(10)
